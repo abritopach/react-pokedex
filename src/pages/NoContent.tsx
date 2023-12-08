000e23e522b57df3f37a57dad7748c9e221d8ec7
Adrián Brito Pacheco <adrbrpa1988@gmail.com>
2023-12-08 13:33:36 +0000

    ✨ Added tailwindcss

--- src/pages/NoContent.tsx
+++ src/pages/NoContent.tsx
@@ -1,7 +1,38 @@
+import { Link } from "react-router-dom";
+
+import PokeApiLogo from '../assets/pokeapi_logo.png';
+import EmptyPokeBall from '../assets/empty_pokeball.png';
+
 export const NoContent = () => {
     return (
-        <>
-            <h1>No content</h1>
-        </>
-    )
+        <section className='flex flex-col justify-center pt-6 items-center w-full mx-auto'>
+            <figure className='flex justify-center items-center pl-4'>
+                <Link to='/' className='flex items-center'>
+                    <span
+                        className='material-icons-outlined mr-2 pt-2 text-4xl'
+                        title='Back to the Menu'
+                    >
+                        reply
+                    </span>
+                    <img
+                        className='w-1/2'
+                        src={PokeApiLogo}
+                        alt='Pokeapi Title'
+                        title='Back to the Menu'
+                    />
+                </Link>
+            </figure>
+            <figure className='flex flex-col justify-center items-center mt-16'>
+                <img
+                    className='w-1/3'
+                    src={EmptyPokeBall}
+                    alt='Empty Pokeball'
+                />
+                <figcaption className='w-4/5 text-center md:text-2xl text-medium text-xl pt-4'>
+                    I'm sorry, we're working on this content, please come back
+                    later.
+                </figcaption>
+            </figure>
+        </section>
+    );
 }
